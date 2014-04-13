519212c5531ef3f95d267ea2b76210544395381c
Brentwood Reid <bwoodreid@gmail.com>
2014-04-13 07:38:13 -0700

    refactor title in application layout to use the application helper

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,2 +1,14 @@
 require 'spec_helper'
 
+describe "Pages" do
+	
+	subject { page }
+
+	describe "Home page" do
+		before { visit root_path }
+
+		# it { should have_selector()}
+		it { should have_title "Sublime Learning" }
+	end
+
+end
