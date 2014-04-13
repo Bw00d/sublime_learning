5d3bfa4e6c355ada07c98fc747afb4788741274e
Brentwood Reid <bwoodreid@gmail.com>
2014-04-13 00:10:18 -0700

    add features folder and update spec_helper for capybara integration testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -38,5 +39,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
+  config.include FactoryGirl::Syntax::Methods
+  config.include ActionDispatch::TestProcess
   config.order = "random"
 end
