151127d7a3a3650ea9cb7b9616e8adf13580a348
Brentwood Reid <bwoodreid@gmail.com>
2014-04-26 11:04:21 -0700

    Add button to reset editor after submit.
    Add buttons hidden buttons to editors.
    Add jQuery to refresh browser on click of reset button.

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,4 +1,35 @@
 
+var editor2 = ace.edit("editor2");
+editor2.getSession().setUseWorker(false);
+editor2.setTheme("ace/theme/idle_fingers");
+editor2.getSession().setMode("ace/mode/ruby");
+editor2.gotoLine(5);
+editor2.getSession().setTabSize(2);
+
+editor2.commands.addCommand({
+	name: 'swap_line_up',
+	bindKey: { mac: 'Ctrl-Command-Up'},
+	exec: function(editor2) {
+	     editor2.moveLinesUp();
+	},
+	readOnly: false // false if this command should not apply in readOnly mode
+});
+
+editor2.commands.addCommand({
+	name: 'swap_line_down',
+	bindKey: { mac: 'Ctrl-Command-Down'},
+	exec: function(editor2) {
+	     editor.moveLinesDown();
+	},
+	readOnly: false // false if this command should not apply in readOnly mode
+});
+
+editor2.getSession().on('change', function(e) {
+
+});
+
+// This editor will be active first
+
 var editor = ace.edit("editor");
 editor.getSession().setUseWorker(false);
 editor.setTheme("ace/theme/idle_fingers");
@@ -25,34 +56,5 @@ editor.commands.addCommand({
 });
 
 editor.getSession().on('change', function(e) {
-
-});
-
-var editor2 = ace.edit("editor2");
-editor2.getSession().setUseWorker(false);
-editor2.setTheme("ace/theme/idle_fingers");
-editor2.getSession().setMode("ace/mode/ruby");
-editor2.gotoLine(5);
-editor2.getSession().setTabSize(2);
-
-editor.commands.addCommand({
-	name: 'swap_line_up',
-	bindKey: { mac: 'Ctrl-Command-Up'},
-	exec: function(editor2) {
-	     editor2.moveLinesUp();
-	},
-	readOnly: false // false if this command should not apply in readOnly mode
-});
-
-editor2.commands.addCommand({
-	name: 'swap_line_down',
-	bindKey: { mac: 'Ctrl-Command-Down'},
-	exec: function(editor2) {
-	     editor.moveLinesDown();
-	},
-	readOnly: false // false if this command should not apply in readOnly mode
-});
-
-editor2.getSession().on('change', function(e) {
 
 });
--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -1,24 +1,46 @@
 $(document).ready(function() {
+	// first editor
 	$('#editor-button').click( function() {
+		$('#editor-button').hide();
+		$('#reset-button').show();
 		input = editor.getValue().trim();
 		answer = $('#answer-block').html().trim();
 		console.log(answer);
 		console.log(input);
 		result = answer.localeCompare(input);
-		$('#editor-button').val('Reset');
 		if (result == 0 ) {
 			alert("That's correct")
 		} else {
 			alert("Try again")
 		}
 	});
-});
+	$('#reset-button').click( function() {
+		$('#reset-button').hide();
+		$('#editor-button').show();
+		location.reload();
 
-// $("#text-search").on("input", function() {
-//     $("#find").val('Clear');  
-// });
-// $("#find").click( function() {
-//     $('#feeder_box').removeHighlight();
-//     $('#text-search').val('');
-//     $("#find").val("Find");
-// });
+	});
+
+	// second editor
+	$('#editor-button2').click( function() {
+		$('#editor-button2').hide();
+		$('#reset-button2').show();
+		input = editor2.getValue().trim();
+		answer = $('#answer-block').html().trim();
+		console.log(answer);
+		console.log(input);
+		result = answer.localeCompare(input);
+		$('#editor-button2').val('Reset');
+		if (result == 0 ) {
+			alert("That's correct")
+		} else {
+			alert("Try again")
+		}
+	});
+	$('#reset-button2').click( function() {
+		$('#reset-button2').hide();
+		$('#editor-button2').show();
+		location.reload();
+	});
+
+});
