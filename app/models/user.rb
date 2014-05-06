02c80b14e44d2481fe776fb5503bc24b22317f68
Brentwood Reid <bwoodreid@gmail.com>
2014-05-06 10:03:59 -0700

    Create remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,21 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   validates :password_digest, presence: true
+
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+ 
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
 describe User do
+	before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
+  subject { @user }
+  
 	it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 	
 end
