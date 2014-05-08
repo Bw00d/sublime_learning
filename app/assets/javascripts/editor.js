c1159eda50398ba04e1fb0bf4ffc984c81722c7b
Brentwood Reid <bwoodreid@gmail.com>
2014-05-08 14:25:55 -0700

    Fix offcanvas load non editor pages
    Pages that did not have a text editor where causing the offcanvas to malfunciton in production
    Moving js files around to make sure everything loads at the correct time seemed to be the solution

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,29 +1,29 @@
-var line2 = $('#line-number2').text();
-var editor2 = ace.edit("editor2");
+// var line2 = $('#line-number2').text();
+// var editor2 = ace.edit("editor2");
 
-editor2.getSession().setUseWorker(false);
-editor2.setTheme("ace/theme/idle_fingers");
-editor2.getSession().setMode("ace/mode/ruby");
-editor2.gotoLine(line2);
-editor2.getSession().setTabSize(2);
+// editor2.getSession().setUseWorker(false);
+// editor2.setTheme("ace/theme/idle_fingers");
+// editor2.getSession().setMode("ace/mode/ruby");
+// editor2.gotoLine(line2);
+// editor2.getSession().setTabSize(2);
 
-//keybindings are in their own files.
+// //keybindings are in their own files.
 
-editor2.getSession().on('change', function(e) {
+// editor2.getSession().on('change', function(e) {
 
-});
+// });
 
-// This editor will be active first
-var line = $('#line-number').text();
-var editor = ace.edit("editor");
-editor.getSession().setUseWorker(false);
-editor.setTheme("ace/theme/idle_fingers");
-editor.getSession().setMode("ace/mode/ruby");
-editor.gotoLine(line);
-editor.getSession().setTabSize(2);
+// // This editor will be active first
+// var line = $('#line-number').text();
+// var editor = ace.edit("editor");
+// editor.getSession().setUseWorker(false);
+// editor.setTheme("ace/theme/idle_fingers");
+// editor.getSession().setMode("ace/mode/ruby");
+// editor.gotoLine(line);
+// editor.getSession().setTabSize(2);
 
-//keybindings are in their own files.
+// //keybindings are in their own files.
 
-editor.getSession().on('change', function(e) {
+// editor.getSession().on('change', function(e) {
 
-});
+// });
--- app/assets/javascripts/editor2_keybindings.js
+++ app/assets/javascripts/editor2_keybindings.js
@@ -1,87 +1,87 @@
-var editor2 = ace.edit("editor2");
+// var editor2 = ace.edit("editor2");
 
-editor2.commands.addCommand({
-	name: 'swap_line_up',
-	bindKey: { win: 'Ctrl-Shift-Up', mac: 'Ctrl-Command-Up'},
-	exec: function(editor2) {
-	     editor2.moveLinesUp();
-	},
-	readOnly: false // false if this command should not apply in readOnly mode
-});
-editor2.commands.addCommand({
-	name: 'swap_line_down',
-	bindKey: { win: 'Ctrl-Shift-Down', mac: 'Ctrl-Command-Down'},
-	exec: function(editor2) {
-	     editor2.moveLinesDown();
-	},
-	readOnly: false // false if this command should not apply in readOnly mode
-});
-editor2.commands.addCommand({
-	name: 'add_line_down',
-	bindKey: { mac: 'Command-Return'},
-	exec: function(editor2) {
-	     editor2.navigateLineEnd()
-	     editor2.splitLine();
-	     editor2.navigateDown(1);
-	},
-	readOnly: false // false if this command should not apply in readOnly mode
-});
-editor2.commands.addCommand({
-	name: 'add_line_above',
-	bindKey: { mac: 'Shift-Command-Return'},
-	exec: function(editor2) {
-	     editor2.navigateLineStart()
-	     editor2.splitLine();
-	},
-	readOnly: false // false if this command should not apply in readOnly mode
-});
-editor2.commands.addCommand({
-	name: 'indent',
-	bindKey: { win: 'Ctrl-]', mac: 'Command-]'},
-	exec: function(editor2) {
-	      editor2.indent()
-	},
-	readOnly: false // false if this command should not apply in readOnly mode
-});
-editor2.commands.addCommand({
-	name: 'unindent',
-	bindKey: { win: 'Ctrl-[', mac: 'Command-['},
-	exec: function(editor2) {
-	      editor2.blockOutdent()
-	},
-	readOnly: false // false if this command should not apply in readOnly mode
-});
-editor2.commands.addCommand({
-	name: 'delete_line',
-	bindKey: { win: 'Ctrl-Shift-k', mac: 'Shift-Ctrl-k'},
-	exec: function(editor2) {
-	      editor2.removeLines()
-	},
-	readOnly: false // false if this command should not apply in readOnly mode
-});
-editor2.commands.addCommand({
-	name: 'delete_to_end_of_line',
-	bindKey: { win: 'Ctrl-k', mac: 'Ctrl-k'},
-	exec: function(editor2) {
-	      editor2.removeToLineEnd()
-	},
-	readOnly: false // false if this command should not apply in readOnly mode
-});
-editor2.commands.addCommand({
-	name: 'toggle_block_comment',
-	bindKey: { win: 'Ctrl-Shift-/', mac: 'Command-Option-/'},
-	exec: function(editor2) {
-	      editor2.toggleBlockComment()
-	},
-	readOnly: false // false if this command should not apply in readOnly mode
-});
 // editor2.commands.addCommand({
-// 	name: 'duplicate_lines',
-// 	bindKey: { win: 'Ctrl+Shift+d', mac: 'Command+Shift+d'},
+// 	name: 'swap_line_up',
+// 	bindKey: { win: 'Ctrl-Shift-Up', mac: 'Ctrl-Command-Up'},
 // 	exec: function(editor2) {
-	      
+// 	     editor2.moveLinesUp();
 // 	},
 // 	readOnly: false // false if this command should not apply in readOnly mode
 // });
+// editor2.commands.addCommand({
+// 	name: 'swap_line_down',
+// 	bindKey: { win: 'Ctrl-Shift-Down', mac: 'Ctrl-Command-Down'},
+// 	exec: function(editor2) {
+// 	     editor2.moveLinesDown();
+// 	},
+// 	readOnly: false // false if this command should not apply in readOnly mode
+// });
+// editor2.commands.addCommand({
+// 	name: 'add_line_down',
+// 	bindKey: { mac: 'Command-Return'},
+// 	exec: function(editor2) {
+// 	     editor2.navigateLineEnd()
+// 	     editor2.splitLine();
+// 	     editor2.navigateDown(1);
+// 	},
+// 	readOnly: false // false if this command should not apply in readOnly mode
+// });
+// editor2.commands.addCommand({
+// 	name: 'add_line_above',
+// 	bindKey: { mac: 'Shift-Command-Return'},
+// 	exec: function(editor2) {
+// 	     editor2.navigateLineStart()
+// 	     editor2.splitLine();
+// 	},
+// 	readOnly: false // false if this command should not apply in readOnly mode
+// });
+// editor2.commands.addCommand({
+// 	name: 'indent',
+// 	bindKey: { win: 'Ctrl-]', mac: 'Command-]'},
+// 	exec: function(editor2) {
+// 	      editor2.indent()
+// 	},
+// 	readOnly: false // false if this command should not apply in readOnly mode
+// });
+// editor2.commands.addCommand({
+// 	name: 'unindent',
+// 	bindKey: { win: 'Ctrl-[', mac: 'Command-['},
+// 	exec: function(editor2) {
+// 	      editor2.blockOutdent()
+// 	},
+// 	readOnly: false // false if this command should not apply in readOnly mode
+// });
+// editor2.commands.addCommand({
+// 	name: 'delete_line',
+// 	bindKey: { win: 'Ctrl-Shift-k', mac: 'Shift-Ctrl-k'},
+// 	exec: function(editor2) {
+// 	      editor2.removeLines()
+// 	},
+// 	readOnly: false // false if this command should not apply in readOnly mode
+// });
+// editor2.commands.addCommand({
+// 	name: 'delete_to_end_of_line',
+// 	bindKey: { win: 'Ctrl-k', mac: 'Ctrl-k'},
+// 	exec: function(editor2) {
+// 	      editor2.removeToLineEnd()
+// 	},
+// 	readOnly: false // false if this command should not apply in readOnly mode
+// });
+// editor2.commands.addCommand({
+// 	name: 'toggle_block_comment',
+// 	bindKey: { win: 'Ctrl-Shift-/', mac: 'Command-Option-/'},
+// 	exec: function(editor2) {
+// 	      editor2.toggleBlockComment()
+// 	},
+// 	readOnly: false // false if this command should not apply in readOnly mode
+// });
+// // editor2.commands.addCommand({
+// // 	name: 'duplicate_lines',
+// // 	bindKey: { win: 'Ctrl+Shift+d', mac: 'Command+Shift+d'},
+// // 	exec: function(editor2) {
+	      
+// // 	},
+// // 	readOnly: false // false if this command should not apply in readOnly mode
+// // });
 
 
