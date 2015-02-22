585d3ae60a377b285e6ce5d7e0dfe7e8e8e8fe40
Brentwood Reid <bwoodreid@gmail.com>
2015-02-22 09:30:26 -0800

    Change admin email and password and make sure it is adding properly in gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ namespace :db do
 	desc "Fill database with sample data"
 	task populate: :environment do
 		User.create!(name: "Admin",
-		email: "admin@example.org",
-		password: "foobar",
-		password_confirmation: "foobar",
+		email: "bwood@gmail.com",
+		password: "Pr32c0tt",
+		password_confirmation: "Pr32c0tt",
 		admin: true)
 	end
 end
